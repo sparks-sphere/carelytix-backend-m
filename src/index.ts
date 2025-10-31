58e543e7dc1eb169e9b01fe5ebef4d857dc2858e
CoderNandan <nandanindia1947@gmail.com>
2025-10-31 23:51:36 +0530

    deploy origin set

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://carelytix-frontend.vercel.app"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
